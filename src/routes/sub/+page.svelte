e025d845cd0adcc68ce5c93de39eaac7e958209b
yashmithaa <syash.mitha94@gmail.com>
2024-04-19 21:43:44 +0530

    fluidsub

--- src/routes/sub/+page.svelte
+++ src/routes/sub/+page.svelte
@@ -1,5 +1,26 @@
 <script></script>
-<h1 class="text-4xl text-red-700" >Subsection here</h1> <!--ctrl+space-->
-<style>
-    
+<head></head>
+<body>
+
+<div class="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow">
+    <!-- fixed-width -->
+    <div class="w-fixed w-full flex-shrink flex-grow-0 px-4">
+        <div class="sticky top-0 p-4 w-full h-full">
+            
+        </div>
+    </div>
+    <main class="w-full flex-grow pt-1 px-3">
+        <!-- fluid-width: main content goes here -->
+        <h1 class="text-4xl text-red-700" >Subsection here</h1> <!--ctrl+space-->
+
+    </main>
+    <div class="w-fixed w-full flex-shrink flex-grow-0 px-2">
+        <!-- fixed-width -->
+        <div class="flex sm:flex-col px-2">
+            <!-- sidebar goes here -->
+        </div>
+    </div>
+</div>
+</body>
+<style> 
 </style>
